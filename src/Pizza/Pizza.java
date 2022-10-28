946944a2305e082b1484e74df344b5f2b59dda0a
ajschlichting <ajschlichting@students.unwsp.edu>
2022-10-27 21:13:45 -0500

    Added Code

---
+++ src/Pizza/Pizza.java
@@ -0,0 +1,21 @@
+package Pizza;
+
+public class Pizza {
+		private String description;
+		private double price;
+		
+		
+		public Pizza(String desc, double pr) {
+			description = desc;
+			price = pr;
+			
+		}
+
+		public void display()
+		{
+			System.out.println(description + " pizza  Price: $" + price);
+		}
+		
+		
+		
+}
